34f100101b4c700ad3025bdca94687bc97bb9d89
Nwogu Michael <mikedbchi@gmail.com>
2022-12-19 07:48:30 +0100

    update

--- src/router/index.js
+++ src/router/index.js
@@ -3,20 +3,30 @@ import GetProductViewVue from "@/views/GetProductView.vue";
 import GetOneProductView from "@/views/GetOneProduct.vue";
 import Cart from "@/views/Cart.vue";
 import PageNotFoundVue from "@/views/PageNotFound.vue";
+import ModalVue from "@/components/Modal.vue";
 const routes = [
   {
     path: "/",
     name: "allProducts",
+    props: true,
     component: GetProductViewVue,
   },
   {
     path: "/cart",
     name: "cart",
+    // props: true,
     component: Cart,
+  },
+  {
+    path: "/modal",
+    name: "modal",
+    props: true,
+    component: ModalVue,
   },
   {
     path: "/product/:id",
     name: "singleProduct",
+    props: true,
     component: GetOneProductView
   },
   {
